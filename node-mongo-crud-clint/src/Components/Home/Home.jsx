e08dec981629e9de66e3e1e36512d2fe5c7a29e1
santo123457 <mdsanto123457@gmail.com>
2023-05-05 17:21:06 +0200

    deleting operation succesfully done

--- node-mongo-crud-clint/src/Components/Home/Home.jsx
+++ node-mongo-crud-clint/src/Components/Home/Home.jsx
@@ -2,31 +2,39 @@ import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Home = () => {
-  const users =useLoaderData();
-  const [displayUser,setDisplayUser] = useState(users);
-  const handleDelete =(user)=>{
+  const users = useLoaderData();
+  const [displayUser, setDisplayUser] = useState(users);
+  const handleDelete = (user) => {
     const agree = window.confirm(`are you ready to delete :${user.name}`);
     if (agree) {
-      fetch(`http://localhost:5000/users/${user._id}`,{
-        method :"DELETE"
+      fetch(`http://localhost:5000/users/${user._id}`, {
+        method: "DELETE",
       })
-      .then(res=>res.json())
-      .then(data=>{
-        if(data.deleteCount >0){
-          alert('user deleted SuccessFully')
-        }
-      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.deletedCount > 0) {
+            alert("user deleted SuccessFully");
+            const remainingUsers = displayUser.filter(
+              (usr) => usr._id !== user._id
+            );
+            setDisplayUser(remainingUsers);
+          }
+        });
     }
-  }
-  return <div>
+  };
+  return (
+    <div>
       <h2> total users {displayUser.length}</h2>
       <div>
-        {
-          displayUser.map(user=><p key={user._id}>name:{user.name} Address: {user.address}  Email: {user.email}   <button onClick={()=>handleDelete(user)}>X</button></p>)
-        }
+        {displayUser.map((user) => (
+          <p key={user._id}>
+            name:{user.name} Address: {user.address} Email: {user.email}{" "}
+            <button onClick={() => handleDelete(user)}>X</button>
+          </p>
+        ))}
       </div>
-
-  </div>;
+    </div>
+  );
 };
 
 export default Home;
--- node-mongo-crud-server/index.js
+++ node-mongo-crud-server/index.js
@@ -44,15 +44,9 @@ async function run() {
       console.log(result);
       res.send(result);
     });
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    // console.log(
-    //   "Pinged your deployment. You successfully connected to MongoDB!"
-    // );
+    
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+    
   }
 }
 run().catch((err) => console.error(err));
